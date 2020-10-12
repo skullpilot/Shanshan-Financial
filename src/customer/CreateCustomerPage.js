88d1804dd461e1f20d69e040a8e86dec38ffeaf3
Maria <xingtao_g@yahoo.ca>
2020-10-12 16:20:29 -0400

    Format CustomerDetailPage

--- src/customer/CreateCustomerPage.js
+++ src/customer/CreateCustomerPage.js
@@ -9,174 +9,175 @@ import Validator from 'validator';
 import { actions } from "../redux/actions";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& .MuiTextField-root": {
-      margin: theme.spacing(2, 5),
-      width: 300,
+    root: {
+        "& .MuiTextField-root": {
+            color: 'red',
+            margin: theme.spacing(2, 5),
+            width: 300,
+        },
+        textAlign: "center",
     },
-    textAlign: "center",
-  },
 }));
 
 function CreateCustomerPage({ createCustomer }) {
-  const classes = useStyles();
-  const [customer, setCustomer] = useState({});
-  const [customerError, setCustomerError] = useState({
-    firstName: { helperText: '', error: false },
-    lastName: { helperText: '', error: false },
-    email: { helperText: '', error: false }
-  });
+    const classes = useStyles();
+    const [customer, setCustomer] = useState({});
+    const [customerError, setCustomerError] = useState({
+        firstName: { helperText: '', error: false },
+        lastName: { helperText: '', error: false },
+        email: { helperText: '', error: false }
+    });
 
-  const setField = Lodash.curry((field, event) => {
-    const { name, value } = event.target;
-    setCustomer((prevState) => ({ ...prevState, [field]: value }));
-  });
+    const setField = Lodash.curry((field, event) => {
+        const { name, value } = event.target;
+        setCustomer((prevState) => ({ ...prevState, [field]: value }));
+    });
 
-  const validate = () => {
-    let isValid = true;
+    const validate = () => {
+        let isValid = true;
 
-    // TODO: 1) Add validations to fields that you think it's necessary to validate
-    //       2) How to refactor these code (@maria)
-    if(!customer.firstName) {
-      isValid = false;
-      setCustomerError((prevState) => ({ ...prevState, firstName: { helperText: 'Please provide first name', error: true } }));
-    } else {
-      setCustomerError((prevState) => ({ ...prevState, firstName: { helperText: '', error: false } }));
+        // TODO: 1) Add validations to fields that you think it's necessary to validate
+        //       2) How to refactor these code (@maria)
+        if (!customer.firstName) {
+            isValid = false;
+            setCustomerError((prevState) => ({ ...prevState, firstName: { helperText: 'Please provide first name', error: true } }));
+        } else {
+            setCustomerError((prevState) => ({ ...prevState, firstName: { helperText: '', error: false } }));
+        }
+
+        if (!customer.lastName) {
+            isValid = false;
+            setCustomerError((prevState) => ({ ...prevState, lastName: { helperText: 'Please provide last name', error: true } }));
+        } else {
+            setCustomerError((prevState) => ({ ...prevState, lastName: { helperText: '', error: false } }));
+        }
+
+        if (!customer.email || !Validator.isEmail(customer.email)) {
+            isValid = false;
+            setCustomerError((prevState) => ({ ...prevState, email: { helperText: 'Please provide email', error: true } }));
+        } else {
+            setCustomerError((prevState) => ({ ...prevState, email: { helperText: '', error: false } }));
+        }
+
+        return isValid;
     }
 
-    if(!customer.lastName) {
-      isValid = false;
-      setCustomerError((prevState) => ({ ...prevState, lastName: { helperText: 'Please provide last name', error: true } }));
-    } else {
-      setCustomerError((prevState) => ({ ...prevState, lastName: { helperText: '', error: false } }));
-    }
-
-    if (!customer.email || !Validator.isEmail(customer.email)) {
-      isValid = false;
-      setCustomerError((prevState) => ({ ...prevState, email: { helperText: 'Please provide email', error: true } }));
-    } else {
-      setCustomerError((prevState) => ({ ...prevState, email: { helperText: '', error: false } }));
-    }
-    
-    return isValid;
-  }
-
-  // TODO: support ui form similar to detail page (@maria)
-  // TODO: add validation support (@maria)
-  // TODO: add specific input support (@maria) eg. date format, phone format
-  return (
-    <div>
-      <div className={classes.root}>
-        <h3>New Customer</h3>
+    // TODO: support ui form similar to detail page (@maria)
+    // TODO: add validation support (@maria)
+    // TODO: add specific input support (@maria) eg. date format, phone format
+    return (
         <div>
-          <TextField
-            label="First Name"
-            variant="outlined"
-            value={customer.firstName || ''}
-            onChange={setField("firstName")}
-            error={customerError.firstName.error}
-            helperText={customerError.firstName.helperText}
-            required
-          />
-          <TextField
-            label="Last Name"
-            variant="outlined"
-            value={customer.lastName || ''}
-            onChange={setField("lastName")}
-            error={customerError.lastName.error}
-            helperText={customerError.lastName.helperText}
-            required
-          />
-          <TextField
-            label="Name"
-            variant="outlined"
-            value={customer.name || ''}
-            onChange={setField("name")}
-          />
-          <TextField
-            label="Email"
-            variant="outlined"
-            value={customer.email || ''}
-            onChange={setField("email")}
-            error={customerError.email.error}
-            helperText={customerError.email.helperText}
-            required
-          />
-          <TextField
-            label="Phone"
-            variant="outlined"
-            value={customer.phone || ''}
-            onChange={setField("phone")}
-          />
-          <TextField
-            label="Birthday"
-            variant="outlined"
-            value={customer.birthday || ''}
-            onChange={setField("birthday")}
-          />
-          <TextField
-            label="Wechat Name"
-            variant="outlined"
-            value={customer.wechatName || ''}
-            onChange={setField("wechatName")}
-          />
-          <TextField
-            label="Wechat ID"
-            variant="outlined"
-            value={customer.wechatId || ''}
-            onChange={setField("wechatId")}
-          />
-          <TextField
-            label="City"
-            variant="outlined"
-            value={customer.city || ''}
-            onChange={setField("city")}
-          />
-          <TextField
-            label="Address"
-            variant="outlined"
-            value={customer.address || ''}
-            onChange={setField("address")}
-          />
-        </div>
-        <div>
-          <TextField
-            label="Notes"
-            variant="outlined"
-            rows={6}
-            multiline
-            value={customer.notes || ''}
-            onChange={setField("notes")}
-            style={{ width: "50%" }} // TODO: support responsive design here, use css media (@maria)
-          />
-        </div>
-        <Button
-          variant="contained"
-          color="primary"
-          style={{ marginTop: "100px", marginBottom: "200px" }}
-          onClick={() => {
-            if (validate()) {
-              createCustomer({
-                id: "100",
-                ...customer,
-              })
-            }
-          }}
-        >
-          Create Customer
+            <div className={classes.root}>
+                <h3>New Customer</h3>
+                <div>
+                    <TextField
+                        label="First Name"
+                        variant="outlined"
+                        value={customer.firstName || ''}
+                        onChange={setField("firstName")}
+                        error={customerError.firstName.error}
+                        helperText={customerError.firstName.helperText}
+                        required
+                    />
+                    <TextField
+                        label="Last Name"
+                        variant="outlined"
+                        value={customer.lastName || ''}
+                        onChange={setField("lastName")}
+                        error={customerError.lastName.error}
+                        helperText={customerError.lastName.helperText}
+                        required
+                    />
+                    <TextField
+                        label="Name"
+                        variant="outlined"
+                        value={customer.name || ''}
+                        onChange={setField("name")}
+                    />
+                    <TextField
+                        label="Email"
+                        variant="outlined"
+                        value={customer.email || ''}
+                        onChange={setField("email")}
+                        error={customerError.email.error}
+                        helperText={customerError.email.helperText}
+                        required
+                    />
+                    <TextField
+                        label="Phone"
+                        variant="outlined"
+                        value={customer.phone || ''}
+                        onChange={setField("phone")}
+                    />
+                    <TextField
+                        label="Birthday"
+                        variant="outlined"
+                        value={customer.birthday || ''}
+                        onChange={setField("birthday")}
+                    />
+                    <TextField
+                        label="Wechat Name"
+                        variant="outlined"
+                        value={customer.wechatName || ''}
+                        onChange={setField("wechatName")}
+                    />
+                    <TextField
+                        label="Wechat ID"
+                        variant="outlined"
+                        value={customer.wechatId || ''}
+                        onChange={setField("wechatId")}
+                    />
+                    <TextField
+                        label="City"
+                        variant="outlined"
+                        value={customer.city || ''}
+                        onChange={setField("city")}
+                    />
+                    <TextField
+                        label="Address"
+                        variant="outlined"
+                        value={customer.address || ''}
+                        onChange={setField("address")}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        label="Notes"
+                        variant="outlined"
+                        rows={6}
+                        multiline
+                        value={customer.notes || ''}
+                        onChange={setField("notes")}
+                        style={{ width: "50%" }} // TODO: support responsive design here, use css media (@maria)
+                    />
+                </div>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ marginTop: "100px", marginBottom: "200px" }}
+                    onClick={() => {
+                        if (validate()) {
+                            createCustomer({
+                                id: "100",
+                                ...customer,
+                            })
+                        }
+                    }}
+                >
+                    Create Customer
         </Button>
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 const actionCreators = {
-  createCustomer: actions.createCustomer,
+    createCustomer: actions.createCustomer,
 };
 
 const ConnectedCreateCustomerPage = connect(
-  null,
-  actionCreators
+    null,
+    actionCreators
 )(CreateCustomerPage);
 
 export default ConnectedCreateCustomerPage;
--- src/customer/CustomerDetailPage.js
+++ src/customer/CustomerDetailPage.js
@@ -4,10 +4,27 @@ import { withRouter } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import * as Lodash from "lodash";
-
+import { makeStyles } from "@material-ui/core/styles";
 import { actions } from "../redux/actions";
 
+const useStyles = makeStyles((theme) => ({
+  TextFieldRoot: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(2, 5),
+      width: 300,
+    },
+    textAlign: "center",
+  },
+  ButtonRoot: {
+    "& .MuiButton-root": {
+      margin: theme.spacing(2, 5),
+      width: 300,
+    },
+  }
+}));
+
 function CustomerDetailPage({ customer, updateCustomer, removeCustomer }) {
+  const classes = useStyles();
   const [customerState, setCustomerState] = useState(customer);
 
   if (!customer) {
@@ -20,89 +37,89 @@ function CustomerDetailPage({ customer, updateCustomer, removeCustomer }) {
   });
 
   return (
-    <div
-      style={{
-        alignContent: "center",
-        display: "flex",
-        flexDirection: "column",
-        width: "500px",
-      }}
+    <div className={classes.TextFieldRoot}
     >
-      <p>{JSON.stringify(customerState)}</p>
-      <TextField
-        label="Name"
-        variant="outlined"
-        value={customerState.name}
-        onChange={setField("name")}
-      />
-      <TextField
-        label="Email"
-        variant="outlined"
-        value={customerState.email}
-        onChange={setField("email")}
-      />
-      <TextField
-        label="Phone"
-        variant="outlined"
-        value={customerState.phone || ''}
-        onChange={setField("phone")}
-      />
-      <TextField
-        label="Birthday"
-        variant="outlined"
-        value={customerState.birthday || ''}
-        onChange={setField("birthday")}
-      />
-      <TextField
-        label="Wechat Name"
-        variant="outlined"
-        value={customerState.wechatName || ''}
-        onChange={setField("wechatName")}
-      />
-      <TextField
-        label="wechat ID"
-        variant="outlined"
-        value={customerState.wechatID || ''}
-        onChange={setField("wechatID")}
-      />
-      <TextField
-        label="City"
-        variant="outlined"
-        value={customerState.city || ''}
-        onChange={setField("city")}
-      />
+      <div>
 
-      <TextField
-        label="Address"
-        variant="outlined"
-        value={customerState.address || ''}
-        onChange={setField("address")}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          updateCustomer(
-            customerState,
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNzZiIsImlhdCI6MTYwMDc0ODcxNiwiZXhwIjoxNjAzMzQwNzE2fQ.1cpvPyQv6fT3qeP2FvuTRUQ4KkkO7pI_atw-KqeGzuo"
-          );
-        }}
-      >
-        Submit
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          removeCustomer(
-            customer.id,
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNzZiIsImlhdCI6MTYwMDc0ODcxNiwiZXhwIjoxNjAzMzQwNzE2fQ.1cpvPyQv6fT3qeP2FvuTRUQ4KkkO7pI_atw-KqeGzuo"
-          );
-        }}
-      >
-        Remove
-      </Button>
+        <TextField
+          label="Name"
+          variant="outlined"
+          value={customerState.name}
+          onChange={setField("name")}
 
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          value={customerState.email}
+          onChange={setField("email")}
+        />
+        <TextField
+          label="Phone"
+          variant="outlined"
+          value={customerState.phone || ''}
+          onChange={setField("phone")}
+        />
+        <TextField
+          label="Birthday"
+          variant="outlined"
+          value={customerState.birthday || ''}
+          onChange={setField("birthday")}
+        />
+        <TextField
+          label="Wechat Name"
+          variant="outlined"
+          value={customerState.wechatName || ''}
+          onChange={setField("wechatName")}
+        />
+        <TextField
+          label="wechat ID"
+          variant="outlined"
+          value={customerState.wechatID || ''}
+          onChange={setField("wechatID")}
+        />
+        <TextField
+          label="City"
+          variant="outlined"
+          value={customerState.city || ''}
+          onChange={setField("city")}
+        />
+
+        <TextField
+          label="Address"
+          variant="outlined"
+          value={customerState.address || ''}
+          onChange={setField("address")}
+        />
+      </div>
+      <div className={classes.ButtonRoot}>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginTop: "100px", marginBottom: "200px" }}
+          onClick={() => {
+            updateCustomer(
+              customerState,
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNzZiIsImlhdCI6MTYwMDc0ODcxNiwiZXhwIjoxNjAzMzQwNzE2fQ.1cpvPyQv6fT3qeP2FvuTRUQ4KkkO7pI_atw-KqeGzuo"
+            );
+          }}
+        >
+          Submit
+      </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginTop: "100px", marginBottom: "200px" }}
+          onClick={() => {
+            removeCustomer(
+              customer.id,
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNzZiIsImlhdCI6MTYwMDc0ODcxNiwiZXhwIjoxNjAzMzQwNzE2fQ.1cpvPyQv6fT3qeP2FvuTRUQ4KkkO7pI_atw-KqeGzuo"
+            );
+          }}
+        >
+          Remove
+      </Button>
+      </div>
     </div>
   );
 }
