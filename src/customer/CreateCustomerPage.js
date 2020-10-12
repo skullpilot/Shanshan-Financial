4ce6f32a66badb44d091f4fe6a25b4ba139c14e4
yucun <yucu@amazon.com>
2020-10-12 01:46:13 -0400

    Add validation in create cx page

--- src/customer/CreateCustomerPage.js
+++ src/customer/CreateCustomerPage.js
@@ -4,6 +4,7 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import * as Lodash from "lodash";
+import Validator from 'validator';
 
 import { actions } from "../redux/actions";
 
@@ -20,20 +21,72 @@ const useStyles = makeStyles((theme) => ({
 function CreateCustomerPage({ createCustomer }) {
   const classes = useStyles();
   const [customer, setCustomer] = useState({});
+  const [customerError, setCustomerError] = useState({
+    firstName: { helperText: '', error: false },
+    lastName: { helperText: '', error: false },
+    email: { helperText: '', error: false }
+  });
 
   const setField = Lodash.curry((field, event) => {
     const { name, value } = event.target;
     setCustomer((prevState) => ({ ...prevState, [field]: value }));
   });
 
+  const validate = () => {
+    let isValid = true;
+
+    // TODO: 1) Add validations to fields that you think it's necessary to validate
+    //       2) How to refactor these code (@maria)
+    if(!customer.firstName) {
+      isValid = false;
+      setCustomerError((prevState) => ({ ...prevState, firstName: { helperText: 'Please provide first name', error: true } }));
+    } else {
+      setCustomerError((prevState) => ({ ...prevState, firstName: { helperText: '', error: false } }));
+    }
+
+    if(!customer.lastName) {
+      isValid = false;
+      setCustomerError((prevState) => ({ ...prevState, lastName: { helperText: 'Please provide last name', error: true } }));
+    } else {
+      setCustomerError((prevState) => ({ ...prevState, lastName: { helperText: '', error: false } }));
+    }
+
+    if (!customer.email || !Validator.isEmail(customer.email)) {
+      isValid = false;
+      setCustomerError((prevState) => ({ ...prevState, email: { helperText: 'Please provide email', error: true } }));
+    } else {
+      setCustomerError((prevState) => ({ ...prevState, email: { helperText: '', error: false } }));
+    }
+    
+    return isValid;
+  }
+
   // TODO: support ui form similar to detail page (@maria)
   // TODO: add validation support (@maria)
-  // TODO: add specific input support (@maria)
+  // TODO: add specific input support (@maria) eg. date format, phone format
   return (
     <div>
       <div className={classes.root}>
         <h3>New Customer</h3>
         <div>
+          <TextField
+            label="First Name"
+            variant="outlined"
+            value={customer.firstName || ''}
+            onChange={setField("firstName")}
+            error={customerError.firstName.error}
+            helperText={customerError.firstName.helperText}
+            required
+          />
+          <TextField
+            label="Last Name"
+            variant="outlined"
+            value={customer.lastName || ''}
+            onChange={setField("lastName")}
+            error={customerError.lastName.error}
+            helperText={customerError.lastName.helperText}
+            required
+          />
           <TextField
             label="Name"
             variant="outlined"
@@ -45,6 +98,9 @@ function CreateCustomerPage({ createCustomer }) {
             variant="outlined"
             value={customer.email || ''}
             onChange={setField("email")}
+            error={customerError.email.error}
+            helperText={customerError.email.helperText}
+            required
           />
           <TextField
             label="Phone"
@@ -97,13 +153,15 @@ function CreateCustomerPage({ createCustomer }) {
         <Button
           variant="contained"
           color="primary"
-          style={{ marginTop: "100px" }}
-          onClick={() =>
-            createCustomer({
-              id: "100",
-              ...customer,
-            })
-          }
+          style={{ marginTop: "100px", marginBottom: "200px" }}
+          onClick={() => {
+            if (validate()) {
+              createCustomer({
+                id: "100",
+                ...customer,
+              })
+            }
+          }}
         >
           Create Customer
         </Button>
