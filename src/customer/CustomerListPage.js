d7f276143e68af95ae231af109c2a9dfb45b4c19
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-11-12 23:25:19 -0500

    delete wechatID and wechatname columns from customerListPage (#32)

--- src/customer/CustomerListPage.js
+++ src/customer/CustomerListPage.js
@@ -2,22 +2,26 @@ import React from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 
-import Table from "../table"
+import Table from "../table";
 
 const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
-  { id: 'phone', numeric: true, disablePadding: false, label: 'Phone Number' },
-  { id: 'email', numeric: true, disablePadding: false, label: 'Email' },
-  { id: 'wechatId', numeric: true, disablePadding: false, label: 'Wechat ID' },
-  { id: 'wechatName', numeric: true, disablePadding: false, label: 'Wechat Name' },
-  { id: 'followUpDate', numeric: true, disablePadding: false, label: 'Followup Date' },
+  { id: "name", numeric: false, disablePadding: true, label: "Name" },
+  { id: "phone", numeric: true, disablePadding: false, label: "Phone Number" },
+  { id: "email", numeric: true, disablePadding: false, label: "Email" },
+  { id: "followUpDate", numeric: true, disablePadding: false, label: "Followup Date" },
 ];
 
 function CustomerListPage({ customers }) {
   const history = useHistory();
 
   return (
-      <Table rows={customers} headCells={headCells} handleItemClick={(id) => history.push(`/customer/${id}`)} handleCreateItem={() => history.push(`/customer`)} createItemText="Create Customer" />
+    <Table
+      rows={customers}
+      headCells={headCells}
+      handleItemClick={(id) => history.push(`/customer/${id}`)}
+      handleCreateItem={() => history.push(`/customer`)}
+      createItemText="Create Customer"
+    />
   );
 }
 
@@ -27,8 +31,6 @@ function mapState(state) {
   };
 }
 
-const ConnectedCustomerListPage = connect(
-  mapState
-)(CustomerListPage);
+const ConnectedCustomerListPage = connect(mapState)(CustomerListPage);
 
 export default ConnectedCustomerListPage;
