1673a39efdfb9a3f02b7438df68c76ecf1720ea6
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2021-03-10 02:22:29 -0500

    update (#66)

--- data-import/modify-dynamodb-date-format.js
+++ data-import/modify-dynamodb-date-format.js
@@ -1,27 +1,27 @@
 const AWS = require("aws-sdk");
 const Fs = require("fs");
-const { constant } = require("lodash");
+// const { constant } = require("lodash");
 
 AWS.config.update({
   region: "us-east-1",
 });
 
 const docClient = new AWS.DynamoDB.DocumentClient();
-const DATA_FILE = "./SSFInsurancePolicies.data";
+const DATA_FILE = 'SSFInsurancePolicies.data';
 
 //--------------------------- Query data --------------------------------------
-const params = {
-  TableName : "SSFInsurancePolicies"
-};
+// const params = {
+//   TableName : "SSFInsurancePolicies"
+// };
 
-docClient.scan(params, function(err, data) {
-  if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-  } else {
-      console.log("Query succeeded.");
-      Fs.writeFileSync(DATA_FILE, JSON.stringify(data.Items, null, 2));
-  }
-});
+// docClient.scan(params, function(err, data) {
+//   if (err) {
+//       console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+//   } else {
+//       console.log("Query succeeded.");
+//       Fs.writeFileSync(DATA_FILE, JSON.stringify(data.Items, null, 2));
+//   }
+// });
 
 //--------------------------- Modify data --------------------------------------
 
@@ -76,6 +76,63 @@ docClient.scan(params, function(err, data) {
 // })
 
 
+//-----------------------------update insurance policy--------------------------------------------
+// Fs.readFile("./SSFInsurancePolicies.data", (err, data) => {
+//   const items = JSON.parse(data);
 
+//   const result = items.map((item) => {
+//     const modified = {...item};
 
+//     if (modified.policyDate && !/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(item.policyDate)) {
+//       const dates = item.policyDate.split("-").map((x) => {
+//         if (x.length > 1) {
+//           return x;
+//         }
+  
+//         return "0" + x;
+//       });
+//       if (dates.length < 3) {
+//         dates.push("01");
+//       }
+      
+//       modified.policyDate = dates.join("-");
+//     }
 
+//     if (modified.applicationDate && !/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(item.applicationDate)) {
+//       const dates = item.applicationDate.split("-").map((x) => {
+//         if (x.length > 1) {
+//           return x;
+//         }
+  
+//         return "0" + x;
+//       });
+//       if (dates.length < 3) {
+//         dates.push("01");
+//       }
+      
+//       modified.applicationDate = dates.join("-");
+//     }
+
+//     return modified;
+//   });
+
+//   Fs.writeFileSync("./cleand-SSFInsurancePolicies.data", JSON.stringify(result, null, 2));
+// });
+
+//----------------------------upload cleaned SSF Insurance policy data--------------------------------------
+Fs.readFile("./cleand-SSFInsurancePolicies.data", (err, data) => {
+  const items = JSON.parse(data);
+  for (let [index, item] of items.entries()) {
+    const params = {
+      TableName: "SSFCustomer",
+      Item: item,
+    };
+    docClient.put(params, function (err, data) {
+      if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("UpdateItem succeeded");
+      }
+    });
+  }
+});
